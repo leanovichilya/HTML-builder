3133283d390656537c517ba2c3439dba3b543cd6
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-05-08 14:03:50 +0300

    refactor: delete some code

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs/promises');
 const path = require('path');
+const pathFolder = path.resolve(__dirname, 'secret-folder');
 
-fs.readdir(path.resolve(__dirname, 'secret-folder'), {withFileTypes: true}, (err) => {
+fs.readdir(pathFolder, {withFileTypes: true}, (err) => {
     if(err) return err.message;
 }).then((files) => {
     const typeFiles = [];
@@ -10,13 +11,12 @@ fs.readdir(path.resolve(__dirname, 'secret-folder'), {withFileTypes: true}, (err
         if (file.isFile()) {
             typeFiles.push(file.name);
         }
-        return typeFiles;
     });
 
     return typeFiles;
 }).then((typeFiles) => {
     typeFiles.forEach((type) => {
-        fs.stat(path.resolve(__dirname, 'secret-folder', type), (err) => {
+        fs.stat(path.resolve(pathFolder, type), (err) => {
         }).then((stats) => {
             console.log([...type.split('.'), (Math.round(stats.size / 1024 * 100) / 100) + 'kb'].join(' - '));
         }).catch(err => console.log(err));
