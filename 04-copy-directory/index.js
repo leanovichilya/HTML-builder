6e4e54d90b1a45344bbbef20d008e50da93da548
Ilya Leanovich <leanovich.ilya@gmail.com>
2023-05-08 16:16:10 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
+const srcPath = path.resolve(__dirname, 'files');
+const destPath = path.resolve(__dirname, 'files-copy');
+
+fs.rm(destPath, {force: true, recursive: true}).then(() => {
+    fs.readdir(srcPath, (err) => {
+        if (err) return err;
+
+    }).then((files) => {
+
+        fs.mkdir(destPath, {recursive: true}, (err) => {
+            if (err) return err;
+        }).then(() => {
+
+            files.forEach((file) => {
+                fs.copyFile(path.resolve(srcPath, file), path.resolve(destPath, file));
+            });
+
+        }).catch((err) => console.log(err.message));
+    }).catch((err) => console.log(err.message));
+});
+
+
 
